dd4318d79b913f11475df798b7b11efc4ccc1e61
Karlen M <karlenmusaelian@gmail.com>
2024-08-08 01:54:39 +0300

    register models

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customers, Category, ClothingItem, Transaction, Review, RentalRequest, Listing
 
-# Register your models here.
+admin.site.register(Customers)
+admin.site.register(Category)
+admin.site.register(ClothingItem)
+admin.site.register(Transaction)
+admin.site.register(Review)
+admin.site.register(RentalRequest)
+admin.site.register(Listing)
